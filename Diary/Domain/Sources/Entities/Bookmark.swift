f295635d69816636aae81784dd013b85867d1f16
first commit <=>
2022-09-15 14:29:47 +0900

    Feat: 북마크 entities 추가

---
+++ Diary/Domain/Sources/Entities/Bookmark.swift
@@ -0,0 +1,25 @@
+//
+//  Bookmark.swift
+//  DomainTests
+//
+//  Created by chuchu on 2022/09/15.
+//
+
+import Foundation
+import CoreLocation
+
+struct Bookmark {
+    typealias Identifier = String
+    
+    let id: Identifier
+    let weather: String
+    let latitude: String
+    let longtitude: String
+    let mood: String
+    let hasWritten: Bool
+    
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: CLLocationDegrees(self.latitude) ?? .zero,
+                                      longitude: CLLocationDegrees(self.longtitude) ?? .zero)
+    }
+}
