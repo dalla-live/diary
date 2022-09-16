4f5affe0303aca0400c13ad456614d30fcc3ae8d
박천송 <qkrcjsthd@gmail.com>
2022-09-16 14:09:49 +0900

    Feat: Weather Case 추가

---
+++ Diary/Repository/Sources/Network/Weather/WeatherCase.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherCase.swift
+//  Repository
+//
+//  Created by cheonsong on 2022/09/16.
+//
+
+import Foundation
+
+enum WeatherCase: String {
+    case thunderstorm
+    case drizzle
+    case rain
+    case snow
+    case atmosphere
+    case clear
+    case clouds
+}
