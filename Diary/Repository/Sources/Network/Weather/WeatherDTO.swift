a281ef2cec28a59fe9052dd6a9e5d2d0e25f9abd
박천송 <qkrcjsthd@gmail.com>
2022-09-16 11:12:43 +0900

    Feat: 현재잘씨정보 API 추가

---
+++ Diary/Repository/Sources/Network/Weather/WeatherDTO.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherDTO.swift
+//  RepositoryTests
+//
+//  Created by cheonsong on 2022/09/16.
+//
+
+import Foundation
+
+public struct WeatherDTO: Codable {
+    var lat: Double // 위도
+    var lon: Double // 경도
+    var appid: String = AppKey.openWeatherMapAppKey // API appkey
+    var lang: String = "kr" // 출력 언어
+}
