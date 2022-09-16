5da162dc050eb7ff7eb4600df1a71156873ccd07
박천송 <qkrcjsthd@gmail.com>
2022-09-16 09:37:33 +0900

    Feat: Reverse Geocoding API 추가 - 위치정보로 도시 이름 얻어오기

--- Diary/Repository/Sources/Network/API/NetworkURL.swift
+++ Diary/Repository/Sources/Network/API/NetworkURL.swift
@@ -9,4 +9,5 @@ import Foundation
 
 struct NetworkURL {
     static let naverUrl = "https://openapi.naver.com" // 파파고 번역 URL
+    static let localUrl = "http://api.openweathermap.org"   // openweathermap.org
 }
