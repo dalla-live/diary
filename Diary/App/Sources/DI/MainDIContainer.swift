f9f847ed438433913d3cf54c3ba17d845ab00e3a
박천송 <qkrcjsthd@gmail.com>
2022-09-19 15:43:06 +0900

    Feat: MainDIContainer 추가 - Dependencies 구현 - MainCoordinator 생성 - BookmarkDIContainer 생성

---
+++ Diary/App/Sources/DI/MainDIContainer.swift
@@ -0,0 +1,40 @@
+//
+//  MainDIContainer.swift
+//  App
+//
+//  Created by cheonsong on 2022/09/19.
+//
+
+import UIKit
+import Foundation
+import Presantation
+
+final class MainDIContainer {
+    
+    public init() {}
+    
+    /// MainCoordinator 생성
+    func makeMainCoordinator(tabBarController: DiaryTabbarController)-> MainCoordinator {
+        return MainCoordinator(coordinator: tabBarController, dependencies: self)
+    }
+    
+    /// Bookmark DI Container 생성
+    public func makeBookmarkDIContainer()-> BookmarkDIContainer {
+        return BookmarkDIContainer()
+    }
+}
+
+extension MainDIContainer: MainCoordinatorDependencies {
+    func makeBookmarkCoordinator(navigationController: UINavigationController) -> BookmarkCoordinator {
+        let diContainer = BookmarkDIContainer()
+        return diContainer.makeBookmarkCoordinator(navigationController: navigationController)
+    }
+    
+    func makeDiaryCoordinator(navigationController: UINavigationController) -> DiaryCoordinator {
+        return DiaryCoordinator(navigation: navigationController)
+    }
+    
+    func makeMapCoordinator(navigationController: UINavigationController) -> MapCoordinator {
+        return MapCoordinator(navigation: navigationController)
+    }
+}
