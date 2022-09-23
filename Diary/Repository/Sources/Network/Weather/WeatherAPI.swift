e29d9e2ac18ece63d70bf79e740ec5b48a444715
박천송 <qkrcjsthd@gmail.com>
2022-09-23 16:52:43 +0900

    Fix: WeatherDTO -> WeatherRequestDTO 네이밍 변경

--- Diary/Repository/Sources/Network/Weather/WeatherAPI.swift
+++ Diary/Repository/Sources/Network/Weather/WeatherAPI.swift
@@ -11,7 +11,7 @@ import SwiftyJSON
 struct WeatherAPI: Networkable {
     typealias Target = WeatherTargetType
     
-    static func requestCurrentWeather(request: WeatherDTO, completion: @escaping (Result<String, Error>) -> Void) {
+    static func requestCurrentWeather(request: WeatherRequestDTO, completion: @escaping (Result<String, Error>) -> Void) {
         
         makeProvider().request(.requestCurrentWeather(request)) { (result) in
             switch ResponseData<JSON>.processJSONResponse(result) {
