01802f837e6406035e2553e63ea596e6943c32d9
박천송 <qkrcjsthd@gmail.com>
2022-09-27 08:42:41 +0900

    Fix: 불필요 파일 제거

--- Diary/Util/Sources/Extensions/String+.swift
+++ Diary/Util/Sources/Extensions/String+.swift
@@ -11,16 +11,4 @@ extension String {
     public var fileName: String {
         self.components(separatedBy: "/").last ?? ""
     }
-    
-    public var localized: String {
-        return LocalizeManager.bundle?.localizedString(forKey: self, value: nil, table: nil) ?? self
-//        return NSLocalizedString(self, comment: "")
-    }
-    
-    /// Format이 있는 로컬라이징 함수 Ex) "My Age %d"
-    /// - Parameter arguments: [CVarArg] format에 들어갈 Value값들
-    /// $d - Int, %f - Float, %ld - Long, %@ - String
-    public func localized(_ arguments: CVarArg = [])-> String {
-        return String(format: self.localized, arguments)
-    }
 }
