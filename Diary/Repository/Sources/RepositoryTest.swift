b2ea1023608b5fb7686af726a84b521ab498221f
박천송 <qkrcjsthd@gmail.com>
2022-09-28 16:56:15 +0900

    Test: BookmarkRepository 테스트코드

--- Diary/Repository/Sources/RepositoryTest.swift
+++ Diary/Repository/Sources/RepositoryTest.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Util
 import RealmSwift
+import Domain
 
 public class RepositoryTest {
     public init() {
@@ -42,27 +43,66 @@ public class RepositoryTest {
 //        })
         
         // MARK: RealmTestCode
-//        let bmManager = DBManager<BM>()
-//        bmManager.add(BM(id: 1, weather: "맑음", location: Coord(), mood: "행복", hasWritten: false))
-//        bmManager.add(BM(id: 2, weather: "흐림", location: Coord(), mood: "우울", hasWritten: false))
-//        bmManager.add(BM(id: 3, weather: "비", location: Coord(), mood: "피곤", hasWritten: false))
+//        let repo = BookmarkRepository(storage: BookmarkStorage())
+//        repo.fetchBookmarkList(query: .month("2022.11"), page: 0, completion: {
+//            Log.d($0)
+//        })
 //
-//        let diaryManager = DBManager<Diary>()
-//        let bookmarks = bmManager.read()
-//        let object = bookmarks.filter("id == 2")
+//        repo.fetchBookmarkList(query: .all, page: 1, completion: { bookmarkList in
+//            Log.d(bookmarkList)
+//        })
+        
+//        repo.updateBookmark(bookmark: Bookmark(id: 12,
+//                                               mood: Mood(mood: .angry),
+//                                               weather: Weather(weather: .atmosphere),
+//                                               date: "0000.00.00",
+//                                               location: Location(lat: 36.003, lon: 127.214, address: "집"),
+//                                               hasWritten: false,
+//                                               note: "업데이트 테스트"), completion: {
+//            switch $0 {
+//            case .success(let model):
+//                Log.d(model)
+//            case .failure(let error):
+//                Log.e(error)
+//            }
+//        })
 //
-//        diaryManager.add(Diary(id: 1, contents: "일기입니다~", bookmark: object.first!))
+//        let bookmark1 = Bookmark(id: 0,
+//                                 mood: Mood(mood: .angry),
+//                                 weather: Weather(weather: .atmosphere),
+//                                 date: "2029.11.3",
+//                                 location: Location(lat: 36.003, lon: 127.214, address: "집"),
+//                                 hasWritten: false,
+//                                 note: "이거는 메모입니다")
 //
-//        let realm = try! Realm()
-//        let objects = realm.objects(BM.self)
+//        let bookmark2 = Bookmark(id: 0,
+//                                 mood: Mood(mood: .amazed),
+//                                 weather: Weather(weather: .drizzle),
+//                                 date: "2021.10.2",
+//                                 location: Location(lat: 36.003, lon: 127.214, address: "집"),
+//                                 hasWritten: false,
+//                                 note: "이거는 메모입니다")
 //
-//        let bm = objects.filter("id == 2").first!
+//        let bookmark3 = Bookmark(id: 0,
+//                                 mood: Mood(mood: .sad),
+//                                 weather: Weather(weather: .clouds),
+//                                 date: "2002.10.2",
+//                                 location: Location(lat: 36.003, lon: 127.214, address: "집"),
+//                                 hasWritten: false,
+//                                 note: "이거는 메모입니다")
 //
-//        try! realm.write {
-//            bm.weather = "천둥"
+//        let bookmark4 = Bookmark(id: 0,
+//                                 mood: Mood(mood: .angry),
+//                                 weather: Weather(weather: .clear),
+//                                 date: "2088.09.2",
+//                                 location: Location(lat: 36.003, lon: 127.214, address: "집"),
+//                                 hasWritten: false,
+//                                 note: "이거는 메모입니다")
+//
+//        [bookmark1, bookmark2, bookmark3, bookmark4].forEach {
+//            repo.addBookmark(bookmark: $0, completion: {
+//                Log.d("Insert Success:: \($0)")
+//            })
 //        }
-//
-//        Log.d(bmManager.read())
-//        Log.d(diaryManager.read())
     }
 }
