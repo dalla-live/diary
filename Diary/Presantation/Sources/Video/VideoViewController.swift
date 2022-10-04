dfb67ce8f00e1547b50a997c0640fe4f8d690e71
정진우 <wlsdn4808@naver.com>
2022-10-04 15:32:27 +0900

    Feat: ImagePickerViewController 추가

--- Diary/Presantation/Sources/Video/VideoViewController.swift
+++ Diary/Presantation/Sources/Video/VideoViewController.swift
@@ -20,20 +20,51 @@ public final class VideoViewController: ProgrammaticallyViewController {
         return vc
     }
     
+    // MARK: Component
+    let addButton = UIButton().then{
+        $0.setImage(UIImage(systemName: "plus.circle.fill"), for: .normal)
+        $0.backgroundColor = .black
+        $0.setTitleColor(.black, for: .normal)
+        $0.addBorder(width: 1, color: .black)
+        $0.layer.cornerRadius = 8
+    }
+    
+    let imagePickerViewController = UIImagePickerController().then{
+        $0.mediaTypes = ["public.movie"]
+    }
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
+        imagePickerViewController.delegate = self
     }
     
     public override func addComponent() {
         fileName = #file.fileName
+        
+        view.addSubview(addButton)
     }
     
     public override func setConstraints() {
-        
+        addButton.snp.makeConstraints{
+            $0.center.equalToSuperview()
+            $0.size.equalTo(70)
+        }
     }
     
     public override func bind() {
+        addButton.rx.tapGesture()
+            .when(.recognized)
+            .bind {[weak self] _ in
+                self?.showAlbum()
+            }.disposed(by: disposeBag)
+    }
+    
+    func showAlbum(){
+        let type = UIImagePickerController.SourceType.photoLibrary
+        guard UIImagePickerController.isSourceTypeAvailable(type) else { return } // 현재 기기에서 가능한지 확인하는 부분
         
+        imagePickerViewController.sourceType = type
+        present(imagePickerViewController, animated: true, completion: nil)
     }
     
     public override func moreAction() {
@@ -46,5 +77,11 @@ public final class VideoViewController: ProgrammaticallyViewController {
     
     deinit {
         print("VideoViewController deinit")
+    }
+}
+
+extension VideoViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("고름")
     }
 }
