d50851e8c88ccce6fd76719a7376507c041962f2
chuyeonhak <=>
2022-10-06 15:21:25 +0900

    Fix: Usecase 접근제어 변경

--- Diary/Domain/Sources/Bookmark/UseCases/LoadBookmarkUseCase.swift
+++ Diary/Domain/Sources/Bookmark/UseCases/LoadBookmarkUseCase.swift
@@ -8,19 +8,19 @@
 import Foundation
 import CoreLocation
 
-protocol LoadBookmarkUseCase {
+public protocol LoadBookmarkUseCase {
     func excute(query: BookmarkQuery, page: Int, completion: @escaping (BookmarkList) -> Void)
     
 }
 
-final class DefaultLoadBookmarkUseCase: LoadBookmarkUseCase {
+public final class DefaultLoadBookmarkUseCase: LoadBookmarkUseCase {
     private let bookmarkRepository: BookmarkRepositoryProtocol
     
-    init(bookmarkRepository: BookmarkRepositoryProtocol) {
+    public init(bookmarkRepository: BookmarkRepositoryProtocol) {
         self.bookmarkRepository = bookmarkRepository
     }
     
-    func excute(query: BookmarkQuery, page: Int, completion: @escaping (BookmarkList) -> Void) {
+    public func excute(query: BookmarkQuery, page: Int, completion: @escaping (BookmarkList) -> Void) {
         bookmarkRepository.fetchBookmarkList(query: query, page: page) { completion($0) }
     }
 }
